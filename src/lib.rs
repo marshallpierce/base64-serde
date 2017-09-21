d182c922bceded55da05ffbf198bbdc3eb3e4025
Marshall Pierce <marshall@mpierce.org>
2017-09-21 17:04:57 -0500

    Better docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,13 @@ pub use serde::{Deserializer, de, Serializer};
 #[doc(hidden)]
 pub use base64::{encode_config, decode_config};
 
-/// Create a module with appropriate `serialize` and `deserialize` functions to use with
-/// serde when specifying how to serialize a particular field, as in `#[serde(with=modname)]`.
+/// Create a type with appropriate `serialize` and `deserialize` functions to use with
+/// serde when specifying how to serialize a particular field.
+///
+/// If you wanted to use the `URL_SAFE_NO_PAD` configuration, for instance, then you might have
+/// `base64_serde_type!(Base64UrlSafeNoPad, URL_SAFE_NO_PAD)` in your code to declare the type, and
+/// then use `#[serde(with = "Base64UrlSafeNoPad")]` on a `Vec<u8>` field that you wished to
+/// serialize to base64 or deserialize from base64.
 #[macro_export]
 macro_rules! base64_serde_type {
     ($typename:ident, $config:expr) => {
