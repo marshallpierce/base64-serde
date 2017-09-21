6daa86cc2f178cb25bd99f8f5f178298d6e5fb1f
Marshall Pierce <marshall@mpierce.org>
2017-09-21 15:01:35 -0500

    pub-use serde and base64 symbols to enable  in macro. Fixes #2.

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,25 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+#[doc(hidden)]
+pub use serde::{Deserializer, de, Serializer};
+#[doc(hidden)]
+pub use base64::{encode_config, decode_config};
+
 macro_rules! base64_serde_type {
     ($typename:ident, $config:expr) => {
         struct $typename {}
 
         impl $typename {
             pub fn serialize<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
-                where S: Serializer {
-                serializer.serialize_str(&base64::encode_config(bytes, $config))
+                where S: $crate::Serializer {
+                serializer.serialize_str(&$crate::encode_config(bytes, $config))
             }
 
             pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
-                where D: Deserializer<'de> {
+                where D: $crate::Deserializer<'de> {
                 let s = <&str>::deserialize(deserializer)?;
-                base64::decode_config(s, $config).map_err(de::Error::custom)
+                $crate::decode_config(s, $config).map_err($crate::de::Error::custom)
             }
         }
     }
@@ -27,10 +32,10 @@ macro_rules! base64_serde_type {
 mod tests {
     extern crate serde_json;
 
-    use super::base64;
-    use super::serde::{Deserialize, Deserializer, de, Serializer};
+    use super::serde::Deserialize;
+    use super::base64::STANDARD;
 
-    base64_serde_type!(Base64Standard, base64::STANDARD);
+    base64_serde_type!(Base64Standard, STANDARD);
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     struct ByteHolder {
@@ -44,7 +49,7 @@ mod tests {
 
         let s = serde_json::to_string(&b).unwrap();
         let expected = format!("{{\"bytes\":\"{}\"}}",
-                               base64::encode_config(&b.bytes, base64::STANDARD));
+                               super::base64::encode_config(&b.bytes, STANDARD));
         assert_eq!(expected, s);
 
         let b2 = serde_json::from_str(&s).unwrap();
